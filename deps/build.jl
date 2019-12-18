6ab739e3c742371e473b4fae2648daae26c0a7a7
Johan Mabille <johan.mabille@gmail.com>
2019-12-18 15:32:20 +0100

    Upgraded to xtensor-julia 0.9.0

--- deps/build.jl
+++ deps/build.jl
@@ -46,13 +46,13 @@ for l in example_labels
 end
 
 # Version of xtl to vendor
-xtl_version = "0.6.5"
+xtl_version = "0.6.9"
 
 # Version of xtensor to vendor
-xtensor_version = "0.20.8"
+xtensor_version = "0.21.2"
 
 # Version of xtensor-julia to vendor
-xtensor_julia_version = "0.8.4"
+xtensor_julia_version = "0.9.0"
 
 xtl_steps = @build_steps begin
   `git clone -b $xtl_version --single-branch https://github.com/QuantStack/xtl $xtl_srcdir`
